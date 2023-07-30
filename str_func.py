9c868031a71cff589b00f132226f9ec7839d459f
Vladimir Byelov <vladimir.byeloff@gmail.com>
2023-07-30 09:17:33 +0300

    docstring added

--- str_func.py
+++ str_func.py
@@ -1,3 +1,9 @@
 def upper_function():
     print(input().upper())
+'''the function is used to capitalise the first letter of strings from input'''
+
+
+def capitalise_function():
+    print(input().upper())
+'''the function is used to capitalise the first letter of words from input'''
 
