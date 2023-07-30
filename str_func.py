cf097cb9fa5e63c002ae8c7a792004afa8b78c89
Vladimir Byelov <vladimir.byeloff@gmail.com>
2023-07-30 10:02:42 +0300

    minimal change coming right up

--- str_func.py
+++ str_func.py
@@ -1,9 +1,9 @@
 def upper_function():
     print(input().upper())
-'''the function is used to capitalise the first letter of strings from input'''
+'''this function is used to capitalise the first letter of strings from input'''
 
 
 def capitalise_function():
     print(input().upper())
-'''the function is used to capitalise the first letter of words from input'''
+'''this function is used to capitalise the first letter of words from input'''
 
